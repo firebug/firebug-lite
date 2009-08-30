7b29b157d60a3d7bc850bb86a91d8fd1aa731de4
pedrosimonetti <pedrosimonetti@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2009-08-30 05:00:08 +0000

    Don't load experimental modules for now.
    git-svn-id: http://fbug.googlecode.com/svn/lite@4160 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- branches/firebug1.3/content/firebug.dev.js
+++ branches/firebug1.3/content/firebug.dev.js
@@ -13,9 +13,9 @@ window.FBDev =
         "firebug/lib.js",
         "firebug/trace.js",
         "firebug/firebug.js",
-        "firebug/domplate.js", // not used yet
+        //"firebug/domplate.js", // not used yet
         "firebug/reps.js",
-        "firebug/reps2.js",  // experimental
+        //"firebug/reps2.js",  // experimental
         "firebug/context.js",
         "firebug/chrome.js",
         "firebug/chrome.injected2.js",
