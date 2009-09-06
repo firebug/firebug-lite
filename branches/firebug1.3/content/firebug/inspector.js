a91aead4ba00c667999ff283a1d24205089f43bd
pedrosimonetti <pedrosimonetti@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2009-09-06 00:07:46 +0000

    Implemented a "crop" method in drawOutline() to avoid triggering the scrollbar when drawing the outline outside the viewport.
    git-svn-id: http://fbug.googlecode.com/svn/lite@4214 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- branches/firebug1.3/content/firebug/inspector.js
+++ branches/firebug1.3/content/firebug/inspector.js
@@ -163,6 +163,93 @@ Firebug.Inspector =
         if (!outlineVisible) this.showOutline();        
     },
     
+    /**
+     * 
+     *   llttttttrr
+     *   llttttttrr
+     *   ll      rr
+     *   ll      rr
+     *   llbbbbbbrr
+     *   llbbbbbbrr
+     */
+    drawOutline: function(el)
+    {
+        var border = 2;
+        var scrollbarSize = 17;
+        
+        var windowSize = Firebug.browser.getWindowSize();
+        var scrollSize = Firebug.browser.getWindowScrollSize();
+        var scrollPosition = Firebug.browser.getWindowScrollPosition();
+        
+        var offsetX = scrollSize.height > windowSize.height ? // is *vertical* scrollbar visible
+                      scrollbarSize :
+                      0;
+        
+        var offsetY = scrollSize.width > windowSize.width ? // is *horizontal* scrollbar visible
+                      scrollbarSize :
+                      0;
+        
+        var box = Firebug.browser.getElementBox(el);
+        
+        var top = box.top;
+        var left = box.left;
+        var height = box.height;
+        var width = box.width;
+        
+        var freeHorizontalSpace = scrollPosition.left + windowSize.width - left - width - offsetX;
+        var freeVerticalSpace = scrollPosition.top + windowSize.height - top - height - offsetY;
+        var numVerticalBorders = freeVerticalSpace > 0 ? 2 : 1;
+        
+        var o = outlineElements;
+        var style;
+        
+        style = o.fbOutlineT.style;
+        style.top = top-border + "px";
+        style.left = left + "px";
+        style.height = border + "px";  // TODO: on initialize()
+        style.width = width + "px";
+  
+        style = o.fbOutlineL.style;
+        style.top = top-border + "px";
+        style.left = left-border + "px";
+        style.height = height+ numVerticalBorders*border + "px";
+        style.width = border + "px";  // TODO: on initialize()
+        
+        style = o.fbOutlineB.style;
+        if (freeVerticalSpace > 0)
+        {
+            style.top = top+height + "px";
+            style.left = left + "px";
+            style.width = width + "px";
+            //style.height = border + "px"; // TODO: on initialize() or worst case?
+        }
+        else
+        {
+            style.top = -2*border + "px";
+            style.left = -2*border + "px";
+            style.width = border + "px";
+            //style.height = border + "px";
+        }
+        
+        style = o.fbOutlineR.style;
+        if (freeHorizontalSpace > 0)
+        {
+            style.top = top-border + "px";
+            style.left = left+width + "px";
+            style.height = height + numVerticalBorders*border + "px";
+            style.width = (freeHorizontalSpace < border ? freeHorizontalSpace : border) + "px";
+        }
+        else
+        {
+            style.top = -2*border + "px";
+            style.left = -2*border + "px";
+            style.height = border + "px";
+            style.width = border + "px";
+        }
+        
+        if (!outlineVisible) this.showOutline();        
+    },
+    
     hideOutline: function()
     {
         if (!outlineVisible) return;
@@ -286,8 +373,8 @@ var inspectContentStyle = inspectStyle + "background: SkyBlue;";
 
 
 var outlineStyle = { 
-    fbHorizontalLine: "background: #3875D7; height: 2px;",
-    fbVerticalLine: "background: #3875D7; width: 2px;"
+    fbHorizontalLine: "background: #3875D7;height: 2px;",
+    fbVerticalLine: "background: #3875D7;width: 2px;"
 }
 
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
