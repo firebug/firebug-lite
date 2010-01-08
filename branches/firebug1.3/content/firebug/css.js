2b0605c80ce19db87a00eca8cf638a3154286c2a
pedrosimonetti <pedrosimonetti@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2010-01-08 01:48:36 +0000

    Implemented the new Style panel, with inline styling editing feature.
    git-svn-id: http://fbug.googlecode.com/svn/lite@5619 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- branches/firebug1.3/content/firebug/css.js
+++ branches/firebug1.3/content/firebug/css.js
@@ -165,7 +165,179 @@ CSSElementPanel.prototype = extend(Firebug.Panel,
     }
 });
 
-Firebug.registerPanel(CSSElementPanel);
+//Firebug.registerPanel(CSSElementPanel);
+
+// ************************************************************************************************
+
+
+// ************************************************************************************************
+// CSS Panel
+
+function CSSEditPanel(){};
+
+CSSEditPanel.prototype = extend(Firebug.Panel,
+{
+    name: "CSSEditPanel",
+    parentPanel: "HTML",
+    title: "Style",
+    
+    options: {
+        hasToolButtons: true
+    },
+
+    create: function()
+    {
+        Firebug.Panel.create.apply(this, arguments);
+        
+        var style = this.contentNode.style;
+        style.margin = "0";
+        //style.padding = "4px 8px";
+        style.fontFamily = "Monaco,monospace";        
+        
+        var doc = Firebug.chrome.document;
+        
+        var el = doc.createElement("pre");
+        el.innerHTML = "element.style {";
+        el.style.margin = "4px 0 0";
+        el.style.padding = "0 0 1px 8px";
+        this.contentNode.appendChild(el);
+        
+        var el = doc.createElement("textarea");
+        el.rows = 1;
+        el.style.width = "80%";
+        //el.style.height = "100%";
+        //el.style.position = "absolute";
+        el.style.fontSize = "11px";
+        el.style.resize = "none";
+        el.style.overflow = "hidden";
+        el.style.outline = "0";
+        el.style.border = "0";
+        el.style.padding = "0";
+        el.style.margin = "0 10%";
+        el.style.color = "darkblue";
+        
+        this.contentNode.appendChild(el);
+        
+        this.editNode = el;
+        
+        var el = doc.createElement("pre");
+        el.innerHTML = "}";
+        el.style.margin = "0";
+        el.style.padding = "0 0 10px 8px";
+        this.contentNode.appendChild(el);
+        
+        var el = doc.createElement("div");
+        el.innerHTML = "Computed Styles";
+        el.style.background = "#eee url(group.gif)";
+        el.style.fontWeight = "bold";
+        el.style.margin = "0 0 4px";
+        el.style.padding = "2px 5px";
+        el.style.border = "1px solid #ccc";
+        el.style.borderWidth = "1px 0";
+        this.contentNode.appendChild(el);
+        
+        var el = doc.createElement("div");
+        this.computedStylesNode = this.contentNode.appendChild(el);
+        
+        this.onKeyUp = bind(this.onKeyUp, this);
+        this.onKeyDown = bind(this.onKeyDown, this);
+        
+        addEvent(this.editNode, "keydown", this.onKeyDown);
+        addEvent(this.editNode, "keyup", this.onKeyUp);
+        
+    },
+    
+    initialize: function()
+    {
+        Firebug.Panel.initialize.apply(this, arguments);
+        
+        var target = documentCache[FirebugChrome.selectedHTMLElementId];
+        if (!target) return;
+        
+        var str = renderStyles2(target);
+        this.editNode.value = str;
+        
+        var str2 = renderStyles(target);
+        this.computedStylesNode.innerHTML = str2.join("");
+        
+        this.adjustEditSize();
+        /*
+        var panel = this;
+        panel.contentNode.innerHTML = str.join("");
+        panel.containerNode.scrollTop = 0;
+        /**/
+    },
+    
+    select: function(node)
+    {
+        var str = renderStyles2(node);
+        this.editNode.value = str;
+        this.adjustEditSize();
+        /*
+        var panel = this;
+        panel.contentNode.innerHTML = str.join("");
+        panel.containerNode.scrollTop = 0;
+        /**/        
+    },
+    
+    adjustEditSize: function(add)
+    {
+        add = add || 0;
+        var nodeValue = this.editNode.value + " ";
+        nodeValue = nodeValue.replace(/\n\r|\r\n/g, "\n");
+        var lines = nodeValue.split(/\n/)
+        var num = lines.length + add
+        this.editNode.rows = num;
+    },
+    
+    onKeyDown: function(event)
+    {
+        if (event.keyCode == 13 /* enter */)
+        {
+            this.adjustEditSize(1);
+        }
+        
+    },
+    
+    onKeyUp: function()
+    {
+        var nodeValue = this.editNode.value;
+        
+        var selectedElement = documentCache[FirebugChrome.selectedHTMLElementId];
+        
+        try
+        {
+            selectedElement.style.cssText = nodeValue;
+        }
+        catch(e)
+        {
+        }
+        
+        this.adjustEditSize();
+        
+        /*
+        var lines = nodeValue.split(/[\n\r]/);
+        
+        var reValue = /\s*([\w-]+):\s*(.*)/;
+        
+        for (var i=0, length=lines.length; i<length; i++)
+        {
+            var line = lines[i];
+            var value = line.match(reValue);
+            
+            if (value)
+            {
+                alert(value[1]);
+            }
+        }
+        /**/
+        
+        //Firebug.Console.log("jhi");
+    }
+});
+
+Firebug.registerPanel(CSSEditPanel);
+
 
 // ************************************************************************************************
 
@@ -253,6 +425,28 @@ var renderStyles = function renderStyles(node)
     }
     
     return str;
+};
+
+var renderStyles2 = function renderStyles(node)
+{
+    var property = ["opacity","filter","azimuth","background","backgroundAttachment","backgroundColor","backgroundImage","backgroundPosition","backgroundRepeat","border","borderCollapse","borderColor","borderSpacing","borderStyle","borderTop","borderRight","borderBottom","borderLeft","borderTopColor","borderRightColor","borderBottomColor","borderLeftColor","borderTopStyle","borderRightStyle","borderBottomStyle","borderLeftStyle","borderTopWidth","borderRightWidth","borderBottomWidth","borderLeftWidth","borderWidth","bottom","captionSide","clear","clip","color","content","counterIncrement","counterReset","cue","cueAfter","cueBefore","cursor","direction","display","elevation","emptyCells","cssFloat","font","fontFamily","fontSize","fontSizeAdjust","fontStretch","fontStyle","fontVariant","fontWeight","height","left","letterSpacing","lineHeight","listStyle","listStyleImage","listStylePosition","listStyleType","margin","marginTop","marginRight","marginBottom","marginLeft","markerOffset","marks","maxHeight","maxWidth","minHeight","minWidth","orphans","outline","outlineColor","outlineStyle","outlineWidth","overflow","padding","paddingTop","paddingRight","paddingBottom","paddingLeft","page","pageBreakAfter","pageBreakBefore","pageBreakInside","pause","pauseAfter","pauseBefore","pitch","pitchRange","playDuring","position","quotes","richness","right","size","speak","speakHeader","speakNumeral","speakPunctuation","speechRate","stress","tableLayout","textAlign","textDecoration","textIndent","textShadow","textTransform","top","unicodeBidi","verticalAlign","visibility","voiceFamily","volume","whiteSpace","widows","width","wordSpacing","zIndex"].sort();
+    
+    var view = node.style;
+
+    var str = [], sl = -1;
+    for(var i=0,len=property.length; i<len; i++)
+    {
+        var item = property[i];
+        if(!view[item]) continue;
+        
+        str[++sl] = toSelectorCase(item);
+        str[++sl] = ": "; 
+        str[++sl] = view[item];
+        str[++sl] = ";\n";
+    }
+    
+    str = str.join("");
+    return str.substr(0, str.length-1);
 };
 
 // ************************************************************************************************
