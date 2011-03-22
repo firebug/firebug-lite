4f562285559c41e73a64c55fad90bf9568503b19
pedrosimonetti@gmail.com <pedrosimonetti@gmail.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2011-03-22 18:23:22 +0000

    Manual merging chrome extension beta changes to lite/branches/firebug1.3
    git-svn-id: http://fbug.googlecode.com/svn/lite@9775 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- branches/firebug1.3/build/chrome-extension-beta/contentScript.js
+++ branches/firebug1.3/build/chrome-extension-beta/contentScript.js
@@ -1,6 +1,11 @@
+// *************************************************************************************************
+
 var isActive = false;
 var isOpen = false;
 var extensionURL = null;
+
+var contextMenuElementXPath = null;
+var isListeningKeyboardActivation = false;
 
 // *************************************************************************************************
 
@@ -26,36 +31,73 @@ var loadStateData = function()
 // load Firebug Lite application
 var loadFirebug = function()
 {
-    if (isOpen)
-        document.documentElement.setAttribute("debug", "true");
-    
+    document.documentElement.setAttribute("debug", isOpen);
+
     injectScriptText("("+listenConsoleCalls+")()");
-    
+
     // TODO: xxxpedro - change to XHR when Issue 41024 is solved
     // Issue 41024: XHR using file: and chrome-extension: protocols not working.
     // http://code.google.com/p/chromium/issues/detail?id=41024
     injectFirebugScript();
 }
 
+// TODO: think a better solution than using the stateData parameter, required
+// by the keyboard activation.
+var loadFirebugAndWait = function(callback, stateData)
+{
+    stateData = stateData || ('1,1,'+extensionURL);
+    localStorage.setItem('Firebug', stateData);
+    loadStateData();
+    chrome.extension.sendRequest({name: isActive ? "FB_enableIcon" : "FB_disableIcon"});
+
+    document.documentElement.setAttribute("debug", isOpen);
+
+    injectFirebugScript();
+
+    setTimeout(function(){
+        waitFirebug(callback);
+    },0);
+};
+
+var waitFirebug = function(callback)
+{
+    if (document && document.getElementById("FirebugChannel"))
+    {
+        stopListeningKeyboardActivation();
+        callback();
+    }
+    else
+        setTimeout(function(){ waitFirebug(callback); }, 100);
+    
+};
+
 // *************************************************************************************************
 
 // inject Firebug Lite script into the page
 var injectFirebugScript = function(url)
 {
-    var script = document.createElement("script");
-    
-    script.src = extensionURL + "firebug-lite-beta.js";
-    script.setAttribute("id", "FirebugLite");
-    script.setAttribute("firebugIgnore", "true");
-    script.setAttribute("extension", "Chrome");
-    document.documentElement.appendChild(script);
-    
-    script.onload = function() {
-        // TODO: xxxpedro remove this files when deploy the new structure
-        script = document.createElement("script");
-        script.src = extensionURL + "googleChrome.js";
+    scriptElement = document.getElementById("FirebugLite");
+    if (scriptElement)
+    {
+        firebugDispatch("FB_toggle");
+    }
+    else
+    {
+        var script = document.createElement("script");
+
+        script.src = extensionURL + "firebug-lite-beta.js";
+        script.setAttribute("id", "FirebugLite");
+        script.setAttribute("firebugIgnore", "true");
+        script.setAttribute("extension", "Chrome");
         document.documentElement.appendChild(script);
-    };
+
+        script.onload = function() {
+            // TODO: xxxpedro remove this files when deploy the new structure
+            script = document.createElement("script");
+            script.src = extensionURL + "googleChrome.js";
+            document.documentElement.appendChild(script);
+        };
+    }
 }
 
 // inject a script into the page
@@ -71,54 +113,6 @@ var injectScriptText = function(text)
     parent.appendChild(script);
     parent.removeChild(script);
 }
-
-// *************************************************************************************************
-
-// listen to console calls before Firebug Lite finishes to load
-var listenConsoleCalls = function()
-{
-    // TODO: xxxpedro add all console functions
-    var fns = ["log", "info", "warn", "error"];
-    
-    var listener = {consoleQueue: ["chromeConsoleQueueHack"]};
-    var queue = listener.consoleQueue;
-    
-    for (var i=0, l=fns.length; i<l; i++)
-    {
-        var fn = fns[i];
-        
-        (function(fn){
-        
-            listener[fn] = function()
-            {
-                queue.push([fn, arguments]);
-            }
-        
-        })(fn);
-    }
-    
-    window.console = listener;
-};
-
-// *************************************************************************************************
-
-var listenKeyboardActivation = function()
-{
-    // TODO: listen to F12 key. if pressed activate Firebug Lite, and open
-    
-    // TODO: this function could also listen to CTRL+SHIFT+C, triggering
-    // Firebug Lite activation, opening it, and starting the inspection,
-    // like in Firebug for Firefox 
-    
-    // TODO: this function should be called also when Firebug Lite is deactivated
-};
-
-var stopListeningKeyboardActivation = function()
-{
-    // TODO: remove listener when Firebug Lite application is activated/loaded
-    
-    // TODO: remove listener on window onunload (if not removed already)
-};
 
 // *************************************************************************************************
 
@@ -139,16 +133,19 @@ chrome.extension.onRequest.addListener
             setTimeout(function(){
             
                 loadStateData();
+
+                //loadFirebug();
+                loadFirebugAndWait(function(){
                 
-                loadFirebug();
-                
-                isActive = true;
-                var message = isActive ? "FB_enableIcon" : "FB_disableIcon";
-                chrome.extension.sendRequest({name: message});
-                
-                loadChannel();
-                
+                    isActive = true;
+                    var message = isActive ? "FB_enableIcon" : "FB_disableIcon";
+                    chrome.extension.sendRequest({name: message});
+
+                    loadChannel();
+                });
+
             },0);
+            
             sendResponse({});
         }
         // handle context menu click by sending "FB_contextMenuClick" message 
@@ -157,8 +154,16 @@ chrome.extension.onRequest.addListener
         {
             // TODO: if not active, activate first, wait the activation to complete
             // and only then dispatch the event to Firebug Lite application
-            
-            firebugDispatch("FB_contextMenuClick");
+            if (isActive)
+                firebugDispatch("FB_contextMenuClick,"+contextMenuElementXPath);
+            else
+                loadFirebugAndWait(function(){
+                    firebugDispatch("FB_contextMenuClick,"+contextMenuElementXPath);
+                });
+        }
+        else if (request.name == "FB_deactivate")
+        {
+            listenKeyboardActivation();
         }
         else
             sendResponse({}); // snub them.
@@ -171,16 +176,23 @@ chrome.extension.onRequest.addListener
 var channel = null;
 var channelEvent;
 
+var onFirebugChannelEvent = function()
+{
+    channel = document.getElementById("FirebugChannel");
+
+    if (channel)
+    {
+        chrome.extension.sendRequest({name: channel.innerText});
+    }
+};
+
 var loadChannel = function()
 {
     channel = document.getElementById("FirebugChannel");
 
     if (channel)
     {
-        channel.addEventListener("FirebugChannelEvent", function() {
-            chrome.extension.sendRequest({name: channel.innerText});
-        });
-        
+        channel.addEventListener("FirebugChannelEvent", onFirebugChannelEvent);
         channelEvent = document.createEvent("Event");
         channelEvent.initEvent("FirebugChannelEvent", true, true);
     }
@@ -188,11 +200,158 @@ var loadChannel = function()
 
 var firebugDispatch = function(data)
 {
-    if (!channel) 
+    if (!channel)
         loadChannel();
-    
+
     channel.innerText = data;
     channel.dispatchEvent(channelEvent);
+};
+
+// *************************************************************************************************
+
+var onContextMenu = function(event)
+{
+    contextMenuElementXPath = getElementXPath(event.target);
+};
+
+var loadListeners = function()
+{
+    window.addEventListener("contextmenu", onContextMenu);
+    window.addEventListener("unload", unloadListeners);
+};
+
+var unloadListeners = function()
+{
+    if (channel)
+    {
+        channel.removeEventListener("FirebugChannelEvent", onFirebugChannelEvent);
+    }
+    
+    window.removeEventListener("contextmenu", onContextMenu);
+    window.removeEventListener("unload", unloadListeners);
+};
+
+// *************************************************************************************************
+
+// listen to console calls before Firebug Lite finishes to load
+var listenConsoleCalls = function()
+{
+    // TODO: xxxpedro add all console functions
+    var fns = ["log", "info", "warn", "error"];
+
+    var listener = {consoleQueue: ["chromeConsoleQueueHack"]};
+    var queue = listener.consoleQueue;
+
+    for (var i=0, l=fns.length; i<l; i++)
+    {
+        var fn = fns[i];
+
+        (function(fn){
+
+            listener[fn] = function()
+            {
+                queue.push([fn, arguments]);
+            }
+
+        })(fn);
+    }
+
+    window.console = listener;
+};
+
+// *************************************************************************************************
+
+var onGlobalKeyDown = function onGlobalKeyDown(event)
+{
+    var keyCode = event.keyCode;
+    var shiftKey = event.shiftKey;
+    var ctrlKey = event.ctrlKey;
+
+    if (keyCode == 123 /* F12 */ && !shiftKey)
+    {
+        loadFirebugAndWait(function(){
+        
+            if (ctrlKey)
+            {
+                firebugDispatch("FB_openInNewWindow");
+            }
+            else
+            {
+                firebugDispatch("FB_toggle");
+            }
+            
+        },"1,0,"); // TODO: think a better solution than using the stateData parameter
+    }
+    else if (keyCode == 67 /* C */ && ctrlKey && shiftKey)
+    {
+        Firebug.Inspector.toggleInspect();
+        //cancelEvent(event, true);
+    }
+    else if (keyCode == 76 /* L */ && ctrlKey && shiftKey)
+    {
+        Firebug.chrome.focusCommandLine();
+        //cancelEvent(event, true);
+    }
+};
+
+var listenKeyboardActivation = function()
+{
+    // TODO: listen to F12 key. if pressed activate Firebug Lite, and open
+
+    // TODO: this function could also listen to CTRL+SHIFT+C, triggering
+    // Firebug Lite activation, opening it, and starting the inspection,
+    // like in Firebug for Firefox
+
+    // TODO: this function should be called also when Firebug Lite is deactivated
+    window.addEventListener("keydown", onGlobalKeyDown);
+    
+    isListeningKeyboardActivation = true;
+};
+
+var stopListeningKeyboardActivation = function()
+{
+    // TODO: remove listener when Firebug Lite application is activated/loaded
+
+    // TODO: remove listener on window onunload (if not removed already)
+    if (isListeningKeyboardActivation)
+    {
+        window.removeEventListener("keydown", onGlobalKeyDown);
+    }
+};
+
+// *************************************************************************************************
+
+var getElementXPath = function(element)
+{
+    if (element && element.id)
+        return '//*[@id="' + element.id + '"]';
+    else
+        return this.getElementTreeXPath(element);
+};
+
+var getElementTreeXPath = function(element)
+{
+    var paths = [];
+
+    for (; element && element.nodeType == 1; element = element.parentNode)
+    {
+        var index = 0;
+        var nodeName = element.nodeName;
+
+        for (var sibling = element.previousSibling; sibling; sibling = sibling.previousSibling)
+        {
+            if (sibling.nodeType != 1) continue;
+
+            if (sibling.nodeName == nodeName)
+                ++index;
+        }
+
+        var tagName = element.nodeName.toLowerCase();
+        var pathIndex = (index ? "[" + (index+1) + "]" : "");
+        paths.splice(0, 0, tagName + pathIndex);
+    }
+
+    return paths.length ? "/" + paths.join("/") : null;
 };
 
 // *************************************************************************************************
@@ -202,14 +361,16 @@ loadStateData();
 
 if (isActive)
 {
-    loadFirebug();
+    loadFirebugAndWait(function(){
+        loadChannel();
+    })
 }
 else
 {
     listenKeyboardActivation();
 }
 
-loadChannel();
+loadListeners();
 
 // *************************************************************************************************
 
