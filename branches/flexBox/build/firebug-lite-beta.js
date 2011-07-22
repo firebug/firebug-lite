296dabfc2cf41374c1ab519fe649741d6e71ebc5
pedrosimonetti@gmail.com <pedrosimonetti@gmail.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2011-07-22 20:20:57 +0000

    [flexBox] Updating build files for FlexBox branch
    git-svn-id: http://fbug.googlecode.com/svn/lite@11360 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- branches/flexBox/build/firebug-lite-beta.js
+++ branches/flexBox/build/firebug-lite-beta.js
@@ -24632,11 +24632,23 @@ var toCamelCaseReplaceFn = function toCamelCaseReplaceFn(m,g)
     return g.toUpperCase();
 };
 
+// ************************************************************************************************
+// StyleSheet Parser
 
+var parseStyleSheet = function parsedStyleSheet()
+{
+    var parsedStyleSheet = {};
+    
+    return parsedStyleSheet;
+};
 
+var getRuleLine = function getRuleLine(rule, parsedStyleSheet)
+{
+};
 
 
 // ************************************************************************************************
+// StyleSheet Analyzer
 
 var ElementCache = Firebug.Lite.Cache.Element;
 var StyleSheetCache = Firebug.Lite.Cache.StyleSheet;
@@ -24862,6 +24874,8 @@ FBL.processAllStyleSheets = function(doc, styleSheetIterator)
 var CSSRuleMap = {};
 var ElementCSSRulesMap = {};
 
+var inlineStyleIndex = -1;
+
 var processStyleSheet = function(doc, styleSheet)
 {
     if (styleSheet.restricted)
@@ -24871,15 +24885,105 @@ var processStyleSheet = function(doc, styleSheet)
     
     var ssid = StyleSheetCache(styleSheet);
     
+    var href = styleSheet.href;
+    
+    var hasCSSParser = typeof CssParser != "undefined";
+    
+    if (hasCSSParser)
+    {
+        // move to CSSParser function
+        if (!href)
+        {
+            // TODO: pre process the position of the inline styles so this will happen only once
+            // in case of having multiple inline styles
+            var index = 0;
+            var ssIndex = ++inlineStyleIndex;
+            var reStyleTag = /\<\s*style.*\>/gi;
+            var reEndStyleTag = /\<\/\s*style.*\>/gi;
+            
+            var source = Firebug.Lite.Proxy.load(Env.browser.location.href);
+            source = source.replace(/\n\r|\r\n/g, "\n"); // normalize line breaks
+            
+            var startLine = 0;
+            
+            do
+            {
+                var matchStyleTag = source.match(reStyleTag); 
+                var i0 = source.indexOf(matchStyleTag[0]) + matchStyleTag[0].length;
+                
+                for (var i=0; i < i0; i++)
+                {
+                    if (source.charAt(i) == "\n")
+                        startLine++;
+                }
+                
+                source = source.substr(i0);
+                
+                index++;
+            }
+            while (index <= ssIndex);
+
+            var matchEndStyleTag = source.match(reEndStyleTag);
+            var i1 = source.indexOf(matchEndStyleTag[0]);
+            
+            var extractedSource = source.substr(0, i1);
+            
+            // TODO merge with block below
+            var parsedObj = CssParser.read(extractedSource, startLine);
+            var parsedRules = parsedObj.children; 
+            var parsedRulesIndex = 0;
+            
+            var dontSupportGroupedRules = isIE && browserVersion < 9;
+            var group = [];
+            var groupItem;
+        }
+        else if (href)
+        {
+            var source = Firebug.Lite.Proxy.load(href);
+            var parsedObj = CssParser.read(source);
+            var parsedRules = parsedObj.children; 
+            var parsedRulesIndex = 0;
+            
+            var dontSupportGroupedRules = isIE && browserVersion < 9;
+            var group = [];
+            var groupItem;
+        }
+    }
+    
     for (var i=0, length=rules.length; i<length; i++)
     {
         var rid = ssid + ":" + i;
         var rule = rules[i];
         var selector = rule.selectorText;
+        var lineNo = null;
         
         if (isIE)
-        {
             selector = selector.replace(reSelectorTag, function(s){return s.toLowerCase();});
+        
+        // TODO: remove this if block
+        if (hasCSSParser)
+        {
+            var parsedRule = parsedRules[parsedRulesIndex];
+            var parsedSelector = parsedRule.selector;
+            
+            if (dontSupportGroupedRules && parsedSelector.indexOf(",") != -1 && group.length == 0)
+                group = parsedSelector.split(",");
+            
+            if (dontSupportGroupedRules && group.length > 0)
+            {
+                groupItem = group.shift();
+                
+                if (CssParser.normalizeSelector(selector) == groupItem)
+                    lineNo = parsedRule.line;
+                
+                if (group.length == 0)
+                    parsedRulesIndex++;
+            }
+            else if (CssParser.normalizeSelector(selector) == parsedRule.selector)
+            {
+                lineNo = parsedRule.line;
+                parsedRulesIndex++;
+            }
         }
         
         // TODO: xxxpedro break grouped rules (,) into individual rules, otherwise
@@ -24893,11 +24997,12 @@ var processStyleSheet = function(doc, styleSheet)
             // because the correct value will depend of the matched element.
             // The proper specificity value for grouped selectors are calculated
             // via getElementCSSRules(element)
-            specificity: selector && selector.indexOf(",") != -1 ? 
+            specificity: selector && selector.indexOf(",") == -1 ? 
                 getCSSRuleSpecificity(selector) : 
                 0,
             
             rule: rule,
+            lineNo: lineNo,
             selector: selector,
             cssText: rule.style ? rule.style.cssText : rule.cssText ? rule.cssText : ""        
         };
@@ -26563,10 +26668,12 @@ CSSElementPanel.prototype = extend(Firebug.CSSStyleSheetPanel.prototype,
 
                 //
                 //var line = domUtils.getRuleLine(rule);
+                // TODO: xxxpedro CSS line number 
+                //var line = ruleData.lineNo;
                 var line;
                 
                 var ruleId = rule.selectorText+"/"+line;
-                var sourceLink = new SourceLink(href, line, "css", rule, instance);
+                var sourceLink = new SourceLink(href, ruleData.lineNo, "css", rule, instance);
 
                 this.markOverridenProps(props, usedProps, inheritMode);
 
@@ -29656,6 +29763,318 @@ TracePanel.prototype = extend(Firebug.Panel,
 });
 
 Firebug.registerPanel(TracePanel);
+
+// ************************************************************************************************
+}});
+
+/* See license.txt for terms of usage */
+
+FBL.ns(function() { with (FBL) {
+// ************************************************************************************************
+
+var CssParser = null;
+
+// ************************************************************************************************
+
+// Simple CSS stylesheet parser from:
+// https://github.com/sergeche/webkit-css
+
+/**
+ * Simple CSS stylesheet parser that remembers rule's lines in file
+ * @author Sergey Chikuyonok (serge.che@gmail.com)
+ * @link http://chikuyonok.ru
+ */
+CssParser = (function(){
+    /**
+     * Returns rule object
+     * @param {Number} start Character index where CSS rule definition starts
+     * @param {Number} body_start Character index where CSS rule's body starts
+     * @param {Number} end Character index where CSS rule definition ends
+     */
+    function rule(start, body_start, end) {
+        return {
+            start: start || 0,
+            body_start: body_start || 0,
+            end: end || 0,
+            line: -1,
+            selector: null,
+            parent: null,
+            
+            /** @type {rule[]} */
+            children: [],
+            
+            addChild: function(start, body_start, end) {
+                var r = rule(start, body_start, end);
+                r.parent = this;
+                this.children.push(r);
+                return r;
+            },
+            /**
+             * Returns last child element
+             * @return {rule}
+             */
+            lastChild: function() {
+                return this.children[this.children.length - 1];
+            }
+        };
+    }
+    
+    /**
+     * Replaces all occurances of substring defined by regexp
+     * @param {String} str
+     * @return {RegExp} re
+     * @return {String}
+     */
+    function removeAll(str, re) {
+        var m;
+        while (m = str.match(re)) {
+            str = str.substring(m[0].length);
+        }
+        
+        return str;
+    }
+    
+    /**
+     * Trims whitespace from the beginning and the end of string
+     * @param {String} str
+     * @return {String}
+     */
+    function trim(str) {
+        return str.replace(/^\s+|\s+$/g, '');
+    }
+    
+    /**
+     * Normalizes CSS rules selector
+     * @param {String} selector
+     */
+    function normalizeSelector(selector) {
+        // remove newlines
+        selector = selector.replace(/[\n\r]/g, ' ');
+        
+        selector = trim(selector);
+        
+        // remove spaces after commas
+        selector = selector.replace(/\s*,\s*/g, ',');
+        
+        return selector;
+    }
+    
+    /**
+     * Preprocesses parsed rules: adjusts char indexes, skipping whitespace and
+     * newlines, saves rule selector, removes comments, etc.
+     * @param {String} text CSS stylesheet
+     * @param {rule} rule_node CSS rule node
+     * @return {rule[]}
+     */
+    function preprocessRules(text, rule_node) {
+        for (var i = 0, il = rule_node.children.length; i < il; i++) {
+            var r = rule_node.children[i],
+                rule_start = text.substring(r.start, r.body_start),
+                cur_len = rule_start.length;
+            
+            // remove newlines for better regexp matching
+            rule_start = rule_start.replace(/[\n\r]/g, ' ');
+            
+            // remove @import rules
+//          rule_start = removeAll(rule_start, /^\s*@import\s*url\((['"])?.+?\1?\)\;?/g);
+            
+            // remove comments
+            rule_start = removeAll(rule_start, /^\s*\/\*.*?\*\/[\s\t]*/);
+            
+            // remove whitespace
+            rule_start = rule_start.replace(/^[\s\t]+/, '');
+            
+            r.start += (cur_len - rule_start.length);
+            r.selector = normalizeSelector(rule_start);
+        }
+        
+        return rule_node;
+    }
+    
+    /**
+     * Saves all lise starting indexes for faster search
+     * @param {String} text CSS stylesheet
+     * @return {Number[]}
+     */
+    function saveLineIndexes(text) {
+        var result = [0],
+            i = 0,
+            il = text.length,
+            ch, ch2;
+            
+        while (i < il) {
+            ch = text.charAt(i);
+            
+            if (ch == '\n' || ch == '\r') {
+                if (ch == '\r' && i < il - 1 && text.charAt(i + 1) == '\n') {
+                    // windows line ending: CRLF. Skip next character 
+                    i++;
+                }
+                
+                result.push(i + 1);
+            }
+            
+            i++;
+        }
+        
+        return result;
+    }
+    
+    /**
+     * Saves line number for parsed rules
+     * @param {String} text CSS stylesheet
+     * @param {rule} rule_node Rule node
+     * @return {rule[]}
+     */
+    function saveLineNumbers(text, rule_node, line_indexes, startLine) {
+        preprocessRules(text, rule_node);
+        
+        startLine = startLine || 0;
+        
+        // remember lines start indexes, preserving line ending characters
+        if (!line_indexes)
+            var line_indexes = saveLineIndexes(text);
+
+        // now find each rule's line
+        for (var i = 0, il = rule_node.children.length; i < il; i++) {
+            var r = rule_node.children[i];
+            r.line = line_indexes.length + startLine;
+            for (var j = 0, jl = line_indexes.length - 1; j < jl; j++) {
+                var line_ix = line_indexes[j];
+                if (r.start >=  line_indexes[j] && r.start <  line_indexes[j + 1]) {
+                    r.line = j + 1 + startLine;
+                    break;
+                }
+            }
+            
+            saveLineNumbers(text, r, line_indexes);
+        }
+        
+        return rule_node;
+    }
+    
+    return {
+        /**
+         * Parses text as CSS stylesheet, remembring each rule position inside 
+         * text
+         * @param {String} text CSS stylesheet to parse
+         */
+        read: function(text, startLine) {
+            var rule_start = [],
+                rule_body_start = [],
+                rules = [],
+                in_comment = 0,
+                root = rule(),
+                cur_parent = root,
+                last_rule = null,
+                stack = [],
+                ch, ch2;
+                
+            stack.last = function() {
+                return this[this.length - 1];
+            };
+            
+            function hasStr(pos, substr) {
+                return text.substr(pos, substr.length) == substr;
+            }
+                
+            for (var i = 0, il = text.length; i < il; i++) {
+                ch = text.charAt(i);
+                ch2 = i < il - 1 ? text.charAt(i + 1) : '';
+                
+                if (!rule_start.length)
+                    rule_start.push(i);
+                    
+                switch (ch) {
+                    case '@':
+                        if (!in_comment) {
+                            if (hasStr(i, '@import')) {
+                                var m = text.substr(i).match(/^@import\s*url\((['"])?.+?\1?\)\;?/);
+                                if (m) {
+                                    cur_parent.addChild(i, i + 7, i + m[0].length);
+                                    i += m[0].length;
+                                    rule_start.pop();
+                                }
+                                break;
+                            }
+                        }
+                    case '/':
+                        if (!in_comment && ch2 == '*') { // comment start
+                            in_comment++;
+                        }
+                        break;
+                        
+                    case '*':
+                        if (ch2 == '/') { // comment end
+                            in_comment--;
+                        }
+                        break;
+                    
+                    case '{':
+                        if (!in_comment) {
+                            rule_body_start.push(i);
+                            
+                            cur_parent = cur_parent.addChild(rule_start.pop());
+                            stack.push(cur_parent);
+                        }
+                        break;
+                        
+                    case '}':
+                        // found the end of the rule
+                        if (!in_comment) {
+                            /** @type {rule} */
+                            var last_rule = stack.pop();
+                            rule_start.pop();
+                            last_rule.body_start = rule_body_start.pop();
+                            last_rule.end = i;
+                            cur_parent = last_rule.parent || root;
+                        }
+                        break;
+                }
+                
+            }
+            
+            return saveLineNumbers(text, root, null, startLine);
+        },
+        
+        normalizeSelector: normalizeSelector,
+        
+        /**
+         * Find matched rule by selector.
+         * @param {rule} rule_node Parsed rule node
+         * @param {String} selector CSS selector
+         * @param {String} source CSS stylesheet source code
+         * 
+         * @return {rule[]|null} Array of matched rules, sorted by priority (most 
+         * recent on top)
+         */
+        findBySelector: function(rule_node, selector, source) {
+            var selector = normalizeSelector(selector),
+                result = [];
+                
+            if (rule_node) {
+                for (var i = 0, il = rule_node.children.length; i < il; i++) {
+                    /** @type {rule} */
+                    var r = rule_node.children[i];
+                    if (r.selector == selector) {
+                        result.push(r);
+                    }
+                }
+            }
+            
+            if (result.length) {
+                return result;
+            } else {
+                return null;
+            }
+        }
+    };
+})();
+
+
+// ************************************************************************************************
+
+FBL.CssParser = CssParser;
 
 // ************************************************************************************************
 }});
