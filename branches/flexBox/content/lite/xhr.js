51d79d1be1e3c916d2ec639831d606a79dc67d2c
pedrosimonetti@gmail.com <pedrosimonetti@gmail.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2011-08-25 19:20:59 +0000

    [flexBox &lite1.4] added FBL.getNativeXHRObject() so Firebug Lite can hide its internal XHR calls
    git-svn-id: http://fbug.googlecode.com/svn/lite@11615 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- branches/flexBox/content/lite/xhr.js
+++ branches/flexBox/content/lite/xhr.js
@@ -505,5 +505,39 @@ if (!isIE6)
     };
 }
 
+//************************************************************************************************
+
+FBL.getNativeXHRObject = function()
+{
+    var xhrObj = false;
+    try
+    {
+        xhrObj = new _XMLHttpRequest();
+    }
+    catch(e)
+    {
+        var progid = [
+                "MSXML2.XMLHTTP.5.0", "MSXML2.XMLHTTP.4.0", 
+                "MSXML2.XMLHTTP.3.0", "MSXML2.XMLHTTP", "Microsoft.XMLHTTP"
+            ];
+          
+        for ( var i=0; i < progid.length; ++i ) {
+            try
+            {
+                xhrObj = new _ActiveXObject(progid[i]);
+            }
+            catch(e)
+            {
+                continue;
+            }
+            break;
+        }
+    }
+    finally
+    {
+        return xhrObj;
+    }
+};
+
 // ************************************************************************************************
 }});
