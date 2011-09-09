a6923f444e3da1c04d1dc98945ddfc8693cf8848
pedrosimonetti@gmail.com <pedrosimonetti@gmail.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2011-09-09 14:13:23 +0000

    [flexBox &lite1.4] jsonViewer will not try to evaluate the contents of the requested file if the content-type is set to "text/plain"
    git-svn-id: http://fbug.googlecode.com/svn/lite@11772 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- branches/flexBox/content/firebug/jsonViewer.js
+++ branches/flexBox/content/firebug/jsonViewer.js
@@ -7,7 +7,8 @@ FBL.ns(function() { with (FBL) {
 // List of JSON content types.
 var contentTypes =
 {
-    // TODO: create issue
+    // TODO: create issue: jsonViewer will not try to evaluate the contents of the requested file 
+    // if the content-type is set to "text/plain"
     //"text/plain": 1,
     "text/javascript": 1,
     "text/x-javascript": 1,
