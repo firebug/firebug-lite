0ef6d97eec45f86874489ecdb6eea44c40d62903
pedrosimonetti@gmail.com <pedrosimonetti@gmail.com@e969d3be-0e28-0410-a27f-dd5c76401a8b>
2011-09-13 17:09:40 +0000

    [flexBox] making the UI popup to work in persistent mode
    git-svn-id: http://fbug.googlecode.com/svn/lite@11846 e969d3be-0e28-0410-a27f-dd5c76401a8b

--- branches/flexBox/content/lite/chromeNew.js
+++ branches/flexBox/content/lite/chromeNew.js
@@ -169,6 +169,8 @@ var createChromeWindow = function(options)
     //* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
     // Locals
 
+    var browserWin = Env.browser.window;
+    var browserContext = new Context(browserWin);
     var prefs = Store.get("FirebugLite");
     var persistedState = prefs && prefs.persistedState || defaultPersistedState;
     
@@ -198,8 +200,6 @@ var createChromeWindow = function(options)
                 node.firebugIgnore = true;
             }
             
-            var browserWin = Env.browser.window;
-            var browserContext = new Context(browserWin);
             var browserWinSize = browserContext.getWindowSize();
             var height = persistedState.height || 300;
             
@@ -315,8 +315,7 @@ var createChromeWindow = function(options)
         else
         {
             var height = persistedState.popupHeight || 300;
-            var browserWin = Firebug.context.window;
-            var browserWinSize = Firebug.context.getWindowSize();
+            var browserWinSize = browserContext.getWindowSize();
             
             var browserWinLeft = typeof browserWin.screenX == "number" ? 
                     browserWin.screenX : browserWin.screenLeft;
